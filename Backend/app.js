e6329b53a531199fae9249631df7011d94920fd0
rexdevin8 <rexdevin8@gmail.com>
2022-10-31 10:32:04 +0530

    Commit 2 by DiyRex

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const tiktok = require("tiktok-down");
 const request = require('request');
+const path = require('path');
+const exp = require('constants');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+// middleware
+
+app.use(express.json());
+app.use(express.urlencoded())
+app.use(express.static(path.join(__dirname, '..', 'Frontend')));
+app.set('view engine', 'html');
 
 var glob_filename = "";
+home_page = path.join(__dirname, '..', 'Frontend', 'index.html');
+download_page = path.join(__dirname, '..', 'Frontend', 'download.html');
+
+
+// Routes 
 
 app.get('/',(req,res) => {
+    res.sendFile(home_page);
+});
+
+app.get('/convert', function(req,res){
+    res.sendFile(home_page, {qs: req.query});
+});
+app.post('/convert', urlencodedParser, function(req,res){
+    console.log(req.body["url-fld"]);
+    res.sendFile(download_page, {qs: req.query});
+});
+
+
+app.get('/about',(req,res) => {
     res.json({ About:'Tiktok Unofficial API',Owner:'DiyRex :)', Release_Date:'10/30/22', Version:'1.0', API_Usage:{ProcessVideo:'localhost:port/processdl?url={tiktok_video_url}',Download_Video:'localhost:port/download?tikurl={url_from_response}'} });
 });
 
@@ -36,4 +66,4 @@ app.get('/download',function(req,res){
     request(req.query.tikurl).pipe(res);
 })
 
-app.listen(port, () => console.log(`App listening on port ${port}`))
+app.listen(port, () => console.log(`App listening on URL http://localhost:${port}`))
